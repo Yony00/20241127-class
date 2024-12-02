faf2cf0694a1e3335ec152359b3a2e40c14d05ed
Yony00 <s1143039@gm.ncue.edu.tw>
2024-12-02 15:03:23 +0800

    Update Split_Map.py

--- pages/Split_Map.py
+++ pages/Split_Map.py
@@ -19,8 +19,8 @@ with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map()
         m.split_map(
-            left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
+            left_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/image%20(1).tif", right_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/tree.tif"
         )
-        m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+        m.add_legend(title="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/image%20(1).tif", builtin_legend="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/tree.tif")
 
 m.to_streamlit(height=700)
