c5c1155ee740d77b309f6b8160e0b61b45cbee16
Yony00 <s1143039@gm.ncue.edu.tw>
2024-12-02 15:12:41 +0800

    Update Split_Map.py

--- pages/Split_Map.py
+++ pages/Split_Map.py
@@ -18,9 +18,9 @@ st.title("Split-panel Map")
 with st.expander("See source code"):
     with st.echo():
         m = leafmap.Map(center=(23.0058,120.2065), zoom=14, height="600px") 
-m.split_map(
-    left_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/image%20(1).tif", right_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/tree.tif"
-)
-m.add_legend(title="Tree cover")
+        m.split_map(
+        left_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/image%20(1).tif", right_layer="https://github.com/Yony00/tiff-to-http2/raw/refs/heads/main/tree.tif"
+        )
+        m.add_legend(title="Tree cover")
 
 m.to_streamlit(height=700)
