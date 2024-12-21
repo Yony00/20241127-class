48e6ffa09f3b8c0d9f46283fe5607784694dabae
Yony00 <s1143039@gm.ncue.edu.tw>
2024-12-21 21:29:36 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.title("Fast Food Restaurants Map with Multiple GeoJSON Sources")
 
 # 定義 GeoJSON 檔案的 URL
 geojson_urls = [
-    "https://raw.githubusercontent.com/Yony00/20241127-class/refs/heads/main/output.geojson",  # 第一間速食餐廳
+    "https://raw.githubusercontent.com/Yony00/20241127-class/refs/heads/main/SB.geojson",  # 第一間速食餐廳
     "https://raw.githubusercontent.com/Yony00/20241127-class/refs/heads/main/restaurant1.geojson",  # 第二間速食餐廳
     "https://raw.githubusercontent.com/Yony00/20241127-class/refs/heads/main/restaurant2.geojson"   # 第三間速食餐廳
 ]
