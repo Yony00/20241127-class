ac0ff7c8e8ca6e22da0a4e1a87b95aef6a1e4310
Yony00 <s1143039@gm.ncue.edu.tw>
2024-12-22 14:57:40 +0800

    Update Subway.py

--- pages/Subway.py
+++ pages/Subway.py
@@ -29,22 +29,3 @@ geojson_url = "https://raw.githubusercontent.com/Yony00/20241127-class/refs/head
 # 使用 requests 下載 GeoJSON 檔案
 response = requests.get(geojson_url)
 
-if response.status_code == 200:
-    # 將下載的資料轉換為 GeoJSON 格式
-    gdf = gpd.read_file(response.text)
-
-    # 初始化地圖，將地圖中心設置為第一個餐廳的位置
-    first_location = gdf.geometry.iloc[0].coords[0]
-    m = folium.Map(location=[first_location[1], first_location[0]], zoom_start=12)
-
-    # 將 GeoJSON 資料加到地圖上
-    folium.GeoJson(gdf).add_to(m)
-
-    # 顯示地圖
-    st_folium(m, width=700)
-
-    # 顯示餐廳列表
-    st.write("Restaurant Locations:")
-    st.write(gdf[['name', 'address']])
-else:
-    st.error("Failed to download GeoJSON file from GitHub.")
